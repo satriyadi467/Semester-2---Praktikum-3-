d97c979c47f039b49942cb8df95c8df5cc92b797
Satria Bagus Trikuncoro <92272548+satriyadi467@users.noreply.github.com>
2022-03-14 23:23:47 +0700

    Add files via upload

---
+++ web dinamis/home.php
@@ -0,0 +1,24 @@
+<?php 
+include_once 'atas.php';
+?>
+
+<h1>SELAMAT DATANG KEMBALI !!! </h1>
+
+<table border="1">
+            <thead>
+                <tr>
+                    <th>no</th>
+                    <th>nama</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Satria Bagus Trikuncoro</td>
+                </tr>
+            </tbody>
+        </table>
+
+<?php 
+require_once 'bawah.php';
+?>
